a5c7a6b36a515f28fbb98d49e5dc49e32444af7a
QiaoqingWu-AC <wu000302@algonquiinlive.com>
2024-11-21 16:00:13 -0500

    Finished basic html/css/js for home page

--- EmpowerAbilityLab.js
+++ EmpowerAbilityLab.js
@@ -3,5 +3,11 @@ const meetBtn = document.getElementById("meetBtn");
 const lightBx = document.getElementById("lightbox");
 
 meetBtn.addEventListener("click", () => {
-  
+  if (lightbox.classList.contains("invisible")) {
+    lightbox.classList.remove("invisible");
+    lightbox.classList.add("visible");
+  } else {
+    lightbox.classList.remove("visible");
+    lightbox.classList.add("invisible");
+  }
 });
